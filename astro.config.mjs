ad848709f4ab584fef4ad6f098663e484a5e5a47
Max DeWeese <maximiliandweese@gmail.com>
2026-06-22 00:15:06 -0700

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://maxdeweese.github.io",
-  base: "/maxdeweese.github.io",
+ //base: "/maxdeweese.github.io",
 });
